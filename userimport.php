4cb9ab9d60e5f6fd2bddd35790f4ca180bb72761
Joshua Koudys <Joshua Koudys>
2014-02-03 21:47:53 -0500

    Support groups on importer

--- userimport.php
+++ userimport.php
@@ -4,7 +4,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
 Loader::model('user_list');
 $nh = Loader::helper('navigation');
 
-function createUser($uName, $uEmail, $uPassword = null, $uAttributes) {
+function createUser($uName, $uEmail, $uPassword = null, $uAttributes, $uGroups) {
   if(null !== UserInfo::getByEmail($uEmail)) {
     echo "User email $uEmail exists.<br />";
   }
@@ -19,12 +19,25 @@ function createUser($uName, $uEmail, $uPassword = null, $uAttributes) {
         'uEmail' => $uEmail,
         'uPassword' => $uPassword,
         'uIsValidated' => LUM_VALIDATE], [UserInfo::ADD_OPTIONS_NOHASH]);
-      foreach($uAttributes as $attHandle=>$attValue) {
+      foreach((array) $uAttributes as $attHandle=>$attValue) {
         // If set to check, validate that the attribute in the 'from' site is in the 'to' site
         if(!LUM_CHECK_ATTRIBUTES || UserAttributeKey::getByHandle($attHandle) ) {
-          $ui->setAttribute($attHandle, $attValue);
+          try {
+            $ui->setAttribute($attHandle, $attValue);
+          } catch(Exception $e) {
+            echo "Error setting attribute $attHandle for $uName. Check that attribute exists in target and is of the correct type.<br />";
+          }
         } else {
           echo "Omitting attribute $attHandle.<br />";
+        }
+      }
+      foreach((array) $uGroups as $gid=>$groupName) {
+        if(null !== ($group = Group::getByName($groupName))) {
+          try {
+            $ui->getUserObject()->enterGroup($group);
+          } catch(Exception $e) {
+            echo "Error adding $uName to $groupName.<br />";
+          }
         }
       }
     }
@@ -38,19 +51,25 @@ if(isset($_REQUEST['xml'])) {
   $doc->loadXML(file_get_contents($_REQUEST['xml']));
   foreach($doc->getElementsByTagName('User') as $user) {
     $uAttributes = array();
-    foreach($user->getElementsByTagName('Attributes')->item(0)->childNodes as $ua) {
+    foreach((array) $user->getElementsByTagName('Attributes')->item(0)->childNodes as $ua) {
       $uAttributes[$ua->tagName] = $ua->textContent;
     }
+    $uGroup = array();
+    foreach((array) $user->getElementsByTagName('Groups')->item(0)->childNodes as $group) {
+      $uGroup[$group->getAttribute('id')] = $group->textContent;
+    }
+
     createUser( $user->getElementsByTagName('name')->item(0)->textContent,
       $user->getElementsByTagName('email')->item(0)->textContent,
       $user->getElementsByTagName('raw_pass')->item(0)->textContent,
-      $uAttributes );
+      $uAttributes,
+      $uGroup );
   }
 }
 else if(isset($_REQUEST['json'])) {
   $json = json_decode(file_get_contents($_REQUEST['json']));
   foreach($json->{'Users'} as $user) {
-    createUser($user->{'name'}, $user->email, $user->raw_pass, $user->{'attributes'});
+    createUser($user->{'name'}, $user->email, $user->raw_pass, $user->{'attributes'}, $user->groups);
   }
 }
 
